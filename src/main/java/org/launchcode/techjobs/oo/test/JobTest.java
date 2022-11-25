c6609de95ce0a0fe17e60a86edc42d0cae60a424
OmarAlsadeq <109781174+OmarAlsadeq@users.noreply.github.com>
2022-11-24 19:21:40 -0600

    "step"

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,39 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+    @Test
+    public void testSettingJobId(){
+        Job job3 =  new Job();
+        Job job2 = new Job();
+        assertNotEquals(job3.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertEquals("Product tester", job.getName());
+        assertEquals("ACME", job.getEmployer().getValue());
+        assertEquals("Desert", job.getLocation().getValue());
+        assertEquals("Quality control", job.getPositionType().getValue());
+        assertEquals("Persistence", job.getCoreCompetency().getValue());
+        assertTrue(job instanceof Job);
+        assertTrue(job.getEmployer() instanceof Employer);
+        assertTrue(job.getLocation() instanceof Location);
+        assertTrue(job.getPositionType() instanceof PositionType);
+        assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+    }
 
 }
